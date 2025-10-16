f832f7f2dba1c0bbcdd56775289d01ff36bbd0c0
flyingfish032 <hussaincme@gmail.com>
2025-10-16 12:33:46 +0530

    Update EcommerceApplicationTests.java

--- src/test/java/com/klu/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/klu/ecommerce/EcommerceApplicationTests.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcommerceApplication {
+public class EcommerceApplicationTests {
     public static void main(String[] args) {
         SpringApplication.run(EcommerceApplication.class, args);
     }
