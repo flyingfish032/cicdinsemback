19682703a17f5044c84868a8302893338a5291a6
flyingfish032 <hussaincme@gmail.com>
2025-10-16 12:29:56 +0530

    Update EcommerceApplicationTests.java

--- src/test/java/com/klu/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/klu/ecommerce/EcommerceApplicationTests.java
@@ -2,7 +2,7 @@ package com.klu.ecommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import com.klu.app.EcommerceApplication; // adjust package as per your project
+import com.klu.ecommerce.EcommerceApplication; // adjust package as per your project
 
 @SpringBootTest(classes = EcommerceApplication.class)
 class EcommerceApplicationTests {
